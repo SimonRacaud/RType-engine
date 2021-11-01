8ecdf34db3e748d2f184f4f2ff362170e1438971
simon.racaud <simon.racaud@epitech.eu>
2021-11-01 17:00:17 +0100

    feat(game-engine): base component class

--- libs/GameEngine/src/BaseComponent/BaseComponent.hpp
+++ libs/GameEngine/src/BaseComponent/BaseComponent.hpp
@@ -8,13 +8,18 @@
 #ifndef BASE_COMPONENT_HPP
 #define BASE_COMPONENT_HPP
 
+#include "Exception/NotRegisteredException.hpp"
 #include <cstddef>
 #include <typeindex>
 #include <typeinfo>
 
+#include "global.hpp"
+
 namespace Engine
 {
     template <class ComponentType> class BaseComponent {
+        friend class ComponentManager;
+
       public:
         BaseComponent() = default;
         virtual ~BaseComponent() = default;
@@ -22,12 +27,35 @@ namespace Engine
         /**
          * \brief unique id of the component
          */
-        static const std::type_index type;
+        static const TypeIdx type;
+
+        static Index getIndex();
+
+      private:
+        static void setIndex(Index index);
+        static Index idx;
     };
 
+    template <class ComponentType>
+    Index BaseComponent<ComponentType>::getIndex()
+    {
+        if (BaseComponent<ComponentType>::idx == InvalidIndex) {
+            throw NotRegisteredException("Component");
+        }
+        return BaseComponent<ComponentType>::idx;
+    }
+    template <class ComponentType>
+    void BaseComponent<ComponentType>::setIndex(Index index)
+    {
+        BaseComponent<ComponentType>::idx = index;
+    }
+
     template <typename ComponentType>
-    const std::type_index BaseComponent<ComponentType>::type = \
-        std::type_index(typeid(BaseComponent<ComponentType>));
+    const std::type_index BaseComponent<ComponentType>::type = std::type_index(
+        typeid(BaseComponent<ComponentType>));
+
+    template <typename ComponentType>
+    Index BaseComponent<ComponentType>::idx = InvalidIndex;
 
 } // namespace Engine
 
