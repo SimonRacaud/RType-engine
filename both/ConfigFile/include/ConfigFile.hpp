1f268443433cb19ca061a6f8415bcaf2ee340164
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-11-01 09:38:37 +0100

    - Add ConfigFile from Singly

---
+++ both/ConfigFile/include/ConfigFile.hpp
@@ -0,0 +1,72 @@
+/*
+** PERSONAL PROJECT, 2021
+** ConfigFile
+** File description:
+** ConfigFile.hpp
+*/
+
+#ifndef CONFIGFILE_HPP
+#define CONFIGFILE_HPP
+
+#include <any>
+#include <vector>
+#include <iostream>
+#include <functional>
+#include <unordered_map>
+
+class ConfigFile {
+  public:
+    ConfigFile(const std::string &filepath, bool defaultInit = true);
+    ConfigFile(const std::vector<std::string> &tab, bool defaultInit = true);
+    ~ConfigFile();
+
+    [[nodiscard]] bool isSetVar(const std::string &name) const;
+    template <typename varType> [[nodiscard]] varType getVar(const std::string &name) const
+    {
+      std::size_t key = typeid(varType).hash_code();
+      auto it = this->_loadLink.find(key);
+
+      if (it != this->_loadLink.end()) {
+        try {
+          return std::any_cast<varType>(this->_loadLink.at(key)(this->getDataFrom(name)));
+        } catch (const std::bad_any_cast &e) {
+          throw std::invalid_argument("Invalide function to load: " + std::string(typeid(varType).name()));
+        }
+      } else
+        throw std::invalid_argument("No getter for: " + std::string(typeid(varType).name()));
+    }
+
+    template <typename varType> void setVarGetter(std::function<std::any (const std::string &)> func, bool force = false)
+    {
+      std::size_t key = typeid(varType).hash_code();
+      auto it = this->_loadLink.find(key);
+
+      if (it != this->_loadLink.end() && !force)
+        throw std::invalid_argument("Getter already exist for: " + std::string(typeid(varType).name()));
+      this->_loadLink[key] = func;
+    }
+
+  private:
+    // GENERIC
+    [[nodiscard]] std::vector<std::string> loadFileContent(const std::string &filepath) const;
+
+    // INTERNAL
+    void emptyManagement(std::string &line) const;
+    void formatManagement(std::string &line) const;
+    void correctInputData(std::string &line) const;
+    void commentManagement(std::string &line) const;
+    void objectInLine(char token_start, char token_end);
+    void globalManagement(const std::vector<std::string> &file);
+
+    // DEFAULT
+    void initDefaultType();
+
+    // PARSING
+    [[nodiscard]] std::string getDataFrom(const std::string &name) const;
+
+  private:
+    std::vector<std::string> _fileContent;
+    std::unordered_map<std::size_t, std::function<std::any (const std::string &)>> _loadLink;
+};
+
+#endif
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -5,6 +5,17 @@
 ** simon
 */
 
-int main(void) {
-  return 0;
+#include "ConfigFile.hpp"
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        return 84;
+    ConfigFile conf(av[1]);
+
+    std::cout << conf.getVar<int>("exampleInt") << std::endl;
+    std::cout << conf.getVar<float>("exampleFloat") << std::endl;
+    std::cout << conf.getVar<std::string>("exampleString") << std::endl;
+    std::cout << (conf.getVar<bool>("exampleBool") ? "true" : "false") << std::endl;
+    return 0;
 }
