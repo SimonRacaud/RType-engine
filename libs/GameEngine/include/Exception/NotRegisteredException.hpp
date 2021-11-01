17677bb840a47bc7f09bcf7fa26f85eb35a0e62c
simon.racaud <simon.racaud@epitech.eu>
2021-11-01 14:25:00 +0100

    feat(game-engine-exception): not registered exception

---
+++ libs/GameEngine/include/Exception/NotRegisteredException.hpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT , 2021
+** FatalErrorException
+** File description:
+** simon
+*/
+
+#ifndef NOTREGISTEREDEXCEPTION_HPP
+#define NOTREGISTEREDEXCEPTION_HPP
+
+#include "BasicException.hpp"
+
+namespace Engine
+{
+    class NotRegisteredException : public BasicException {
+      public:
+        NotRegisteredException(std::string const &type, Index index)
+            : BasicException(
+                type + " " + std::to_string(index) + " not registered")
+        {
+        }
+
+        explicit NotRegisteredException(std::string const &type)
+            : BasicException(type + " not registered")
+        {
+        }
+
+        virtual ~NotRegisteredException() = default;
+    };
+} // namespace Engine
+
+#endif // NOTREGISTEREDEXCEPTION_HPP
