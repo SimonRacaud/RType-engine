5128cdc0c4c96c2699c1eb3bb9039b55d00d8242
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-11-02 15:54:56 +0100

    - Add colors

--- lib/Sfml/include/Interface/ITextManager.hpp
+++ lib/Sfml/include/Interface/ITextManager.hpp
@@ -14,10 +14,21 @@
 class ITextManager
 {
     public:
+        enum class color_e
+        {
+            RED,
+            BLUE,
+            GREEN,
+            BLACK,
+            WHITE,
+            MAX_VALUE
+        };
+    public:
         ~ITextManager() = default;
         // GLOBAL
         virtual const vector2D &getSize() = 0;
         virtual const vector2D &getPosition() = 0;
+        virtual void setColor(const color_e &) = 0;
         virtual void setSize(const vector2D &) = 0;
         virtual void setPosition(const vector2D &) = 0;
         virtual void setContent(const std::string &) = 0;
