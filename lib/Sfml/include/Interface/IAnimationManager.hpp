58fd61e3781e4cc0702c5d15dd9ffb128d03fde2
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-11-02 15:43:04 +0100

    - Add interface

--- lib/Sfml/include/Interface/IAnimationManager.hpp
+++ lib/Sfml/include/Interface/IAnimationManager.hpp
@@ -8,12 +8,21 @@
 #ifndef IANIMATIONMANAGER_HPP
 #define IANIMATIONMANAGER_HPP
 
+#include "Item/vector2D.hpp"
 #include <string>
 
 class IAnimationManager
 {
     public:
         ~IAnimationManager() = default;
+
+        virtual const vector2D &getSize() = 0;
+        virtual const vector2D &getPosition() = 0;
+        virtual void setSize(const vector2D &) = 0;
+        virtual void setPosition(const vector2D &) = 0;
+        virtual void setDirFilepath(const std::string &) = 0;
+
+        virtual void refresh() = 0;
 };
 
 #endif
