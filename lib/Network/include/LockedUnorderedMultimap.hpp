5e65cf623d2366aabcd5fd38ab748d56979c3d0e
Arthur <arthur.jourdan@epitech.eu>
2021-11-03 22:06:32 +0100

    style(CMakeLists.txt): clean src

---
+++ lib/Network/include/LockedUnorderedMultimap.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** LockedUnorderedMap.hpp
+** File description:
+** TODO: add description
+*/
+#ifndef R_TYPE_LOCKED_UNORDERED_MAP_HPP
+#define R_TYPE_LOCKED_UNORDERED_MAP_HPP
+
+template <typename Key, typename Value> class LockedUnorderedMap {
+    LockedUnorderedMap() = default;
+    ~LockedUnorderedMap() = default;
+};
+
+#endif // R_TYPE_LOCKED_UNORDERED_MAP_HPP
