dd3837cb58247f0d1766f1cec53b0190b25bbcdd
simon.racaud <simon.racaud@epitech.eu>
2021-11-04 17:16:00 +0100

    fix(game-engine): system manager, select system and register system

--- libs/GameEngine/src/SystemManager/SystemManager.hpp
+++ libs/GameEngine/src/SystemManager/SystemManager.hpp
@@ -17,11 +17,14 @@
 #include <vector>
 #include "global.hpp"
 #include "AbstractSystem/AbstractSystem.hpp"
-#include "InvalidParameterException.hpp"
-#include "NotFoundException.hpp"
-    
+#include "Exception/InvalidParameterException.hpp"
+#include "Exception/NotFoundException.hpp"
+
 namespace Engine
 {
+    using std::vector;
+    using std::shared_ptr;
+
     class SystemManager {
       public:
         SystemManager() = default;
@@ -29,9 +32,9 @@ namespace Engine
 
         /**
          * @brief Creates and adds a system to the systems vector
-         * 
+         *
          * @tparam SystemType  the type of the system to create
-         * @tparam Args  
+         * @tparam Args
          * @param systemArgs  the arguments to send to the system
          * @return SystemType& a reference to the newly created system
          */
@@ -40,7 +43,7 @@ namespace Engine
 
         /**
          * @brief Removes a system from the vector
-         * 
+         *
          * @tparam SystemType the type of the system to remove
          */
         template <class SystemType>
@@ -48,33 +51,33 @@ namespace Engine
 
         /**
          * @brief Selects a list of systems to add them to the selected vector
-         * 
-         * @tparam Args 
+         *
+         * @tparam Args
          * @param systemTypeList All of the systems to select
          */
-        template <class SystemType, class... Args>
-        void selectSystems(Args&&... systemTypeList);
+        template <class SystemTypeList>
+        void selectSystem();
 
-        template<class SystemType>
+        template<class... SystemTypeList>
         void selectSystems();
 
         /**
          * @brief Executes every selected system
-         * 
+         *
          */
         void executeCycle();
 
         /**
          * @brief For each system that is registered it calls its onEntityUpdated
-         * 
-         * @param entity 
-         * @param signature 
+         *
+         * @param entity
+         * @param signature
          */
         void onEntityUpdated(Entity entity, const Signature &signature);
 
         /**
          * @brief For each system that is registered it calls its onEntityRemoved
-         * 
+         *
          * @param entity the Entity to remove
          */
         void onEntityRemoved(Entity entity);
@@ -82,7 +85,7 @@ namespace Engine
 
         /**
          * @brief Returns a system of type from the list
-         * 
+         *
          * @tparam SystemType type to return
          * @return SystemType& returned reference
          */
@@ -110,35 +113,30 @@ namespace Engine {
 
         if (sys != _systems.end()) {
             std::cerr << "Did not create new system because it already exists" << std::endl;
-            return *sys;
+            return *static_cast<SystemType *>(sys->get());
         }
-        SystemType *newSys = _systems.emplace_back(std::make_unique<SystemType>(std::forward<Args>(systemArgs)...));
-        return *newSys;
+        shared_ptr<IAbstractSystem> newSys = _systems.emplace_back(std::make_unique<SystemType>(std::forward<Args>(systemArgs)...));
+        return *static_cast<SystemType *>(newSys.get());
     }
 
     template <class SystemType>
     void SystemManager::unregisterSystem()
     {
         const TypeIdx type = std::type_index(typeid(SystemType));
-        
+
         _systems.erase(std::remove_if(_systems.begin(), _systems.end(), [&](auto &sysType) {
             return sysType.getType() == type;
         }));
     }
 
-    template <class SystemType, class... Args>
-    void SystemManager::selectSystems(Args&&... systemTypeList)
-    {
-        auto it = retrieveSystem(GET_TYPE_IDX(SystemType));
-
-        if (it == _systems.end())
-            throw NotFoundException("Could not find system to select it");
-        _selectedSystems.push_back(*it);
-        selectSystems(systemTypeList...);
-    }
-    
-    template<class SystemType>
+    template <class... SystemTypeList>
     void SystemManager::selectSystems()
+    {
+        (this->selectSystems<SystemTypeList>(), ...);
+    }
+
+    template<class SystemType>
+    void SystemManager::selectSystem()
     {
         auto it = retrieveSystem(GET_TYPE_IDX(SystemType));
 
