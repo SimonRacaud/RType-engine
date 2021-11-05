4d3725bca7c1e0ae02503e9b419dec2d432b86a0
Arthur <arthur.jourdan@epitech.eu>
2021-11-06 00:27:32 +0100

    test(launchCtest.sh): add script to automatically compile and launch tests

---
+++ lib/Network/tests/functional/launchCtest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR=$( basename `pwd` )
+
+if [[ "$DIR" != "build" ]]; then
+   mkdir -p build && cd build
+fi
+
+if [[ $1 == "conan" ]]; then
+    CONAN_PROFILE="default"
+    if (( $@ >= 2 )); then
+        CONAN_PROFILE=$2
+    fi
+    conan install .. -pr "$CONAN_PROFILE"
+fi
+
+find . -name "CMakeCache\.txt" -type f -delete
+
+cmake .. && make -j 10
+
+ctest
