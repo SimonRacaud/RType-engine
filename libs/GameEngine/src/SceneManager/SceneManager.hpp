721f806f0cd1d6b7696f0b8df5295acb340e36b8
simon.racaud <simon.racaud@epitech.eu>
2021-11-08 19:19:54 +0100

    feat(engine): in scene manager, check template type, and isCurrent method

--- libs/GameEngine/src/SceneManager/SceneManager.hpp
+++ libs/GameEngine/src/SceneManager/SceneManager.hpp
@@ -8,23 +8,23 @@
 #ifndef SCENEMANAGER_HPP_
 #define SCENEMANAGER_HPP_
 
-#include <vector>
-#include <memory>
 #include <algorithm>
+#include <memory>
 #include <stack>
-#include "IScene.hpp"
-#include "EntityManager/EntityManager.hpp"
+#include <vector>
 #include "AbstractScene/AbstractScene.hpp"
-#include "env.hpp"
+#include "Engine/EngineFactory.hpp"
+#include "EntityManager/EntityManager.hpp"
 #include "Exception/InvalidParameterException.hpp"
 #include "Exception/NotRegisteredException.hpp"
-#include "Engine/EngineFactory.hpp"
+#include "IScene.hpp"
+#include "env.hpp"
 
 namespace Engine
 {
     using std::shared_ptr;
-    using std::vector;
     using std::stack;
+    using std::vector;
 
     class SceneManager {
       public:
@@ -34,8 +34,9 @@ namespace Engine
         void run();
 
         /**
-         * @brief Creates a new scene of type from args and adds it to the scenes vector
-         * 
+         * @brief Creates a new scene of type from args and adds it to the
+         * scenes vector
+         *
          * @tparam SceneType Type of the Scene
          * @tparam Args Variadic template
          * @param args Arguments to be passed down to the scene
@@ -44,7 +45,8 @@ namespace Engine
         void registerScene(Args &&...args);
 
         /**
-         * @brief Removes a scene from the vector, and if that scene is the current scene, change it to the first scene
+         * @brief Removes a scene from the vector, and if that scene is the
+         * current scene, change it to the first scene
          * @deprecated
          * @tparam SceneType Type of scene
          */
@@ -53,7 +55,7 @@ namespace Engine
 
         /**
          * @brief Select a scene from the vector and load it
-         * 
+         *
          * @tparam SceneType Type of scene
          * @param closePrevious Bool to enable closing of previous scene
          */
@@ -62,41 +64,47 @@ namespace Engine
 
         void select(const shared_ptr<IScene> &scene, bool closePrevious = true);
 
+        template <class SceneType> bool isCurrent() const;
+
         /**
          * @brief Select the previous scene and load it
-         * 
+         *
          */
         void selectPrevious();
-        
+
         /**
          * @brief Checks if there is a previous scene
-         * 
-         * @return true 
-         * @return false 
+         *
+         * @return true
+         * @return false
          */
         bool havePrevious() const;
-        
+
         /**
          * @brief Get the Current scene
-         * 
-         * @return shared_ptr<IScene> 
+         *
+         * @return shared_ptr<IScene>
          */
         shared_ptr<IScene> getCurrent();
 
         /**
          * @brief Gets the pointer to an abstractScene of type
-         * 
-         * @tparam SceneType 
-         * @return shared_ptr<AbstractScene<SceneType>> 
+         *
+         * @tparam SceneType
+         * @return shared_ptr<AbstractScene<SceneType>>
          */
         template <class SceneType>
         shared_ptr<AbstractScene<SceneType>> get();
 
       private:
         /**
-         * @brief Flush selection changes the current into the next and closes current
+         * @brief Flush selection changes the current into the next and closes
+         * current
          */
         void flushSelection();
+
+        template <class SceneType>
+        void _checkType() const;
 
       private:
         vector<shared_ptr<IScene>>::iterator getSceneItFromType(const TypeIdx &type);
@@ -112,26 +120,32 @@ namespace Engine
     template <typename SceneType, typename... Args>
     void SceneManager::registerScene(Args &&...args)
     {
+        this->_checkType<SceneType>();
+
         const TypeIdx type = AbstractScene<SceneType>::type;
-    
+
         if (getSceneItFromType(type) != _scenes.end())
-            throw FatalErrorException("This scene has already been registered");
-        _scenes.push_back(std::make_shared<SceneType>(std::forward<Args>(args)...));
+            throw FatalErrorException(
+                "This scene has already been registered");
+        _scenes.push_back(
+            std::make_shared<SceneType>(std::forward<Args>(args)...));
     }
 
-    template <class SceneType>
-    void SceneManager::unregister()
+    template <class SceneType> void SceneManager::unregister()
     {
-        throw FatalErrorException("SceneManager::unregister Deprecated method");
+        throw FatalErrorException(
+            "SceneManager::unregister Deprecated method");
         auto it = getSceneItFromType(AbstractScene<SceneType>::type);
 
         if (it == _scenes.end()) {
             throw NotRegisteredException(
                 "No scene with this type has been registered");
         }
-        if (_currentScene != nullptr &&
-            _currentScene->getType().hash_code() == (*it)->getType().hash_code()) {
-            throw InvalidParameterException("Trying to unregister current loaded scene");
+        if (_currentScene != nullptr
+            && _currentScene->getType().hash_code()
+                == (*it)->getType().hash_code()) {
+            throw InvalidParameterException(
+                "Trying to unregister current loaded scene");
         }
         if (_scenes.size() > 1) {
             (*it) = _scenes.back();
@@ -139,27 +153,46 @@ namespace Engine
         _scenes.pop_back();
     }
 
-    template <typename SceneType>
-    void SceneManager::select(bool closePrevious)
+    template <typename SceneType> void SceneManager::select(bool closePrevious)
     {
+        this->_checkType<SceneType>();
+
         auto it = getSceneItFromType(AbstractScene<SceneType>::type);
 
         if (it == _scenes.end())
-            throw NotRegisteredException("Could not select scene that has not been registered");
+            throw NotRegisteredException(
+                "Could not select scene that has not been registered");
         _nextScene = *it;
         this->_closeSceneOnChange = closePrevious;
     }
 
-
     template <class SceneType>
     shared_ptr<AbstractScene<SceneType>> SceneManager::get()
     {
+        this->_checkType<SceneType>();
+
         const TypeIdx type = AbstractScene<SceneType>::type;
         auto it = getSceneItFromType(type);
 
         if (it == _scenes.end())
-            throw InvalidParameterException("Could not get pointer to non existing scene");
+            throw InvalidParameterException(
+                "Could not get pointer to non existing scene");
         return reinterpret_cast<AbstractScene<SceneType>>(*it);
+    }
+
+    template <class SceneType> bool SceneManager::isCurrent() const
+    {
+        this->_checkType<SceneType>();
+         return !(_currentScene == nullptr)
+            && _currentScene->getType().hash_code()
+            == SceneType::type.hash_code();
+    }
+
+    template <class SceneType> void SceneManager::_checkType() const
+    {
+        static_assert(
+            std::is_base_of<AbstractScene<SceneType>, SceneType>::value,
+            "Invalid scene type");
     }
 
 } // namespace Engine
