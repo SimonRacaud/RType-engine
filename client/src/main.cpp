d4354fe79b64a2f1f108ff5a60e0f3ea81871406
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-11-09 16:41:16 +0100

    - Add example Audio

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -6,18 +6,32 @@
 */
 
 #include <iostream>
+#include "global.hpp"
 #include "CustomCluster.hpp"
 #include "CustomEntityName.hpp"
 #include "EngineCore.hpp"
 #include "Scene/Start/StartScene.hpp"
 #include "System/LogPosition/LogPositionSystem.hpp"
 
+#include "Event/AudioEvent/AudioEventLoad.hpp"
+#include "Event/AudioEvent/AudioEventPlay.hpp"
+#include "Event/AudioEvent/AudioEventVolume.hpp"
+
 using namespace std;
 
 Engine::IGameEngine &engine = Engine::EngineFactory::getInstance();
+Engine::Event::EventCallbackRegister *reg = nullptr;
+
+static void init()
+{
+	if (!reg)
+		reg = new Engine::Event::EventCallbackRegister();
+}
 
 int main(void)
 {
+    init();
+
     Engine::ComponentManager &componentManager = engine.getComponentManager();
     componentManager.registerComponent<Engine::Position>();
     componentManager.registerComponent<Engine::Velocity>();
@@ -30,6 +44,11 @@ int main(void)
     sceneManager.registerScene<Scene::StartScene>();
     sceneManager.select<Scene::StartScene>();
 
+    reg->registerEvent<AudioEventLoad>(AudioEventLoad::audioType_e::MUSIC, "asset/music/song.ogg");
+    reg->registerEvent<AudioEventVolume>("asset/music/song.ogg", 100);
+    reg->registerEvent<AudioEventPlay>("asset/music/song.ogg");
+
+    reg->execQueue();
     engine.exec();
     return EXIT_SUCCESS;
 }
