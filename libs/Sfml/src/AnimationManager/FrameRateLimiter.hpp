4b3bc96a9ffcd99ec49836ee8281aabc221f5a2b
Pol BACHELIN <pol.bachelin@epitech.eu>
2021-11-11 04:32:12 -0800

    fix(FrameRateLimiter): rewrite of FrameRateLimiter to use std::chrono

--- libs/Sfml/src/AnimationManager/FrameRateLimiter.hpp
+++ libs/Sfml/src/AnimationManager/FrameRateLimiter.hpp
@@ -9,6 +9,7 @@
 #define FRAMERATELIMITER_HPP
 
 #include <functional>
+#include <chrono>
 
 namespace Singly
 {
@@ -18,7 +19,7 @@ namespace Singly
         public:
             FrameRateLimiter(float frameRate, std::function<functionType> func) : _billion(1000000000), _frameRate(frameRate), _func(func)
             {
-                clock_gettime(CLOCK_REALTIME, &this->_lastReference);
+                _lastReference = std::chrono::high_resolution_clock::now();
             }
             ~FrameRateLimiter() = default;
 
@@ -34,21 +35,25 @@ namespace Singly
                 size_t nb_it = 0;
                 size_t nb_sec = 0;
                 long double nb_nsec = 0;
-                struct timespec actual{};
+                auto start = std::chrono::high_resolution_clock::now();
 
-                clock_gettime(CLOCK_REALTIME, &actual);
-                nb_sec = actual.tv_sec - this->_lastReference.tv_sec;
-                nb_nsec = actual.tv_nsec - this->_lastReference.tv_nsec;
+                //nb_sec = std::chrono::time_point_cast<std::chrono::seconds>(
+                    //start - _lastReference);
+                nb_sec = std::chrono::duration_cast<std::chrono::seconds>(start.time_since_epoch()
+                    - _lastReference.time_since_epoch()).count();
+               
+                nb_nsec = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                    start.time_since_epoch() - _lastReference.time_since_epoch()).count();
                 nb_it = (nb_sec + (nb_nsec / this->_billion)) * this->_frameRate;
                 if (nb_it != 0)
-                    clock_gettime(CLOCK_REALTIME, &this->_lastReference);
+                    _lastReference = std::chrono::high_resolution_clock::now();
                 return nb_it;
             }
 
         private:
             size_t _billion;
             float _frameRate;
-            struct timespec _lastReference;
+            std::chrono::steady_clock::time_point _lastReference;
             std::function<functionType> _func;
     };
 };
