877c294d6cc5ebf3a66f8d191699647e69bce47b
simon.racaud <simon.racaud@epitech.eu>
2021-11-12 10:36:13 +0100

    feat(network-tram): component sync tram serializer

--- lib/Network/include/Tram/ComponentSync.hpp
+++ lib/Network/include/Tram/ComponentSync.hpp
@@ -12,33 +12,56 @@
 #define COMPONENTSYNC_HPP
 
 #include <inttypes.h>
+#include <cstring>
+#include <stdexcept>
 #include <stddef.h>
 #include "utils/timeDef.hpp"
 #include <typeindex>
+#include "ISerializable.hpp"
 
-namespace Tram {
-    struct ComponentSync {
-        /**
-         * @brief network id of the entity
-         */
-        uint32_t networkId;
-        /**
-         * @brief action timestamp (rollback)
-         */
-        Time timestamp;
-        /**
-         * @brief Component type
-         */
-        size_t componentType;
-        /**
-         * @brief size of the component
-         */
-        size_t size;
-        /**
-         * @brief component serialized struct
-         */
-        void *component;
-    };
+namespace Network
+{
+    namespace Tram
+    {
+        #define COMP_SYNC_HEAD_SIZE (sizeof(uint32_t) + sizeof(Time) + sizeof(size_t) * 3) \
+            + sizeof(void *)
+
+        class ComponentSync : public ISerializable<ComponentSync> {
+          public:
+            ComponentSync() = default;
+            virtual ~ComponentSync();
+
+            /**
+              * @brief total size
+             */
+            size_t size;
+            /**
+              * @brief network id of the entity
+             */
+            uint32_t networkId;
+            /**
+              * @brief action timestamp (rollback)
+             */
+            Time timestamp;
+            /**
+              * @brief Component type
+             */
+            size_t componentType;
+            /**
+              * @brief size of the component
+             */
+            size_t componentSize;
+            /**
+              * @brief component serialized struct
+             */
+            void *component;
+            // <- after deserializing, the component is after in memory
+
+            virtual uint8_t *deserialize();
+            virtual void serialize(uint8_t *buffer);
+            ComponentSync &operator=(ComponentSync &other);
+        };
+    }
 }
 
 #endif // COMPONENTSYNC_HPP
---
+++ lib/Network/src/Tram/ComponentSync.cpp
@@ -0,0 +1,61 @@
+/*
+** EPITECH PROJECT , 2021
+** ComponentSync
+** File description:
+** \file ComponentSync.cpp
+** \author simon
+** \brief
+** \date 12/11/2021
+*/
+
+#include "Tram/ComponentSync.hpp"
+
+using namespace Network::Tram;
+
+ComponentSync::~ComponentSync()
+{
+    if (component != nullptr) {
+        delete static_cast<uint8_t *>(component);
+        component = nullptr;
+    }
+}
+
+uint8_t *ComponentSync::deserialize()
+{
+    this->size = COMP_SYNC_HEAD_SIZE + this->componentSize;
+    if (this->component == nullptr) {
+        throw std::logic_error("ComponentSync::deserialize null component");
+    }
+    uint8_t *buffer = new uint8_t[this->size];
+    ComponentSync *ptr = reinterpret_cast<ComponentSync *>(buffer);
+
+    std::memcpy(buffer, this, COMP_SYNC_HEAD_SIZE); // copy header
+    ptr->component = static_cast<void *>(buffer + COMP_SYNC_HEAD_SIZE); // update header ptr
+    std::memcpy(ptr->component, this->component, this->componentSize); // copy body
+    return buffer;
+}
+
+void ComponentSync::serialize(uint8_t *buffer)
+{
+    ComponentSync *ptr = reinterpret_cast<ComponentSync *>(buffer);
+
+    *this = *ptr;
+}
+
+ComponentSync &ComponentSync::operator=(ComponentSync &other)
+{
+    // copy header
+    this->size = other.size;
+    this->networkId = other.networkId;
+    this->timestamp = other.timestamp;
+    this->componentType = other.componentType;
+    this->componentSize = other.componentSize;
+    if (other.component == nullptr) {
+        this->component = nullptr;
+    } else {
+        this->component = new uint8_t[this->componentSize]; // alloc body
+        std::memcpy(this->component, other.component, this->componentSize); // copy body
+    }
+    return *this;
+}
+
