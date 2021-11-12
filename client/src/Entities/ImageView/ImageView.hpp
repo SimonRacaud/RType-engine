4d07c521530d88fbcdb220c1e24ad76dbc54856b
simon.racaud <simon.racaud@epitech.eu>
2021-11-12 21:43:57 +0100

    feat(client-component): image view component, show a sprite

---
+++ client/src/Entities/ImageView/ImageView.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT , 2021
+** ImageView
+** File description:
+** \file ImageView.hpp
+** \author simon
+** \brief
+** \date 12/11/2021
+*/
+
+#ifndef IMAGEVIEW_HPP
+#define IMAGEVIEW_HPP
+
+#include <string>
+#include "../../../../libs/Sfml/include/Item/vector2f.hpp"
+#include "CustomCluster.hpp"
+#include "Item/vector2D.hpp"
+
+class ImageView {
+  public:
+    ImageView(
+        const std::string &path,
+        const vector2D &position,
+        const vector2f &scale,
+        Engine::ClusterName cluster
+        );
+    virtual ~ImageView() = default;
+};
+
+#endif // IMAGEVIEW_HPP
