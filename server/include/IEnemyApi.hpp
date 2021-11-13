6a1c2143237172e74d1763e9f09b10d56c45d5bd
Pol Bachelin <pol.bachelin@epitech.eu>
2021-11-13 19:23:54 +0100

    feat(IEnemyApi): enemy api for developping shared enemy libs

---
+++ server/include/IEnemyApi.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2021
+** RType
+** File description:
+** IEnemyApi
+*/
+
+#ifndef IENEMYAPI_HPP_
+#define IENEMYAPI_HPP_
+
+#include "EngineCore.hpp"
+#include "Item/vector2D.hpp"
+
+class IEnemyApi {
+	public:
+		virtual ~IEnemyApi() = 0;
+		
+		virtual void idle(const Engine::Entity e) = 0;
+		virtual void move(const Engine::Entity e) = 0;
+		virtual void attack(const Engine::Entity e) = 0;
+		virtual float getMoveSpeed() const = 0;
+		virtual float getShootingSpeed() const = 0;
+		virtual int getHealth() const = 0;
+		virtual std::string getSpriteSheet() const = 0;
+		virtual int getAnimationNbFrames() const = 0;
+		virtual vector2D getIdleAnimPos() const = 0;
+		virtual vector2D getMoveAnimPos() const = 0;
+		virtual vector2D getAttackAnimPos() const = 0;
+};
+
+#endif /* !IEnemyApi_HPP_ */
