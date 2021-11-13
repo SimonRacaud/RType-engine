cd3ed7cb8dcea21f3ae0977a951f68769448558d
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-11-13 12:49:15 +0100

    - Add Component, System, Scene manager in server core

--- server/src/ServerCore/ServerCore.cpp
+++ server/src/ServerCore/ServerCore.cpp
@@ -25,4 +25,20 @@ ServerCore::~ServerCore()
 
 void ServerCore::run(void)
 {
+    Engine::ComponentManager &componentManager = engine.getComponentManager();
+    componentManager.registerComponent<Engine::Timer>();
+    componentManager.registerComponent<Engine::Hitbox>();
+    componentManager.registerComponent<Engine::Position>();
+    componentManager.registerComponent<Engine::Velocity>();
+    componentManager.registerComponent<Engine::ScoreComponent>();
+
+    Engine::SystemManager &systemManager = engine.getSystemManager();
+    systemManager.registerSystem<Engine::TimerSystem>();
+    systemManager.registerSystem<Engine::PhysicsSystem>();
+    systemManager.registerSystem<Engine::ColliderSystem>();
+
+    Engine::SceneManager &sceneManager = engine.getSceneManager();
+    //sceneManager.registerScene<Scene::Example>();
+
+    engine.exec();
 }
