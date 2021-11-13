9676a8234de5c4f9c05b05fdb2e51199440db54b
simon.racaud <simon.racaud@epitech.eu>
2021-11-13 16:07:59 +0100

    feat(game-scene): room list scene, gui skeleton

--- client/src/Scene/RoomList/RoomListScene.cpp
+++ client/src/Scene/RoomList/RoomListScene.cpp
@@ -9,7 +9,19 @@
 */
 
 #include "RoomListScene.hpp"
+
 #include "GameCore/GameCore.hpp"
+#include "Item/vector2D.hpp"
+
+#include "Entities/Button/Button.hpp"
+#include "Entities/Label/Label.hpp"
+#include "Entities/ImageView/ImageView.hpp"
+
+#include "Event/GUI/SelectPreviousScene.hpp"
+#include "Event/GUI/SelectScene.hpp"
+
+#include "System/RenderSystem/RenderSystem.hpp"
+#include "System/InputEventSystem/InputEventSystem.hpp"
 
 using namespace Scene;
 using namespace Engine;
@@ -21,8 +33,27 @@ RoomListScene::RoomListScene()
 
 void RoomListScene::open()
 {
-    // <== create entities
+    const std::string backgroundPath = GameCore::config->getVar<std::string>("HOME_BACKGROUND");
+    const std::string listBackgroundPath = GameCore::config->getVar<std::string>("ROOM_BACKGROUND");
+    const std::string mention = GameCore::config->getVar<std::string>("HOME_MENTIONS");
+    // ENTITY CREATION
+    // header
+    ImageView background(backgroundPath, vector2D(0, 0), vector2f(1, 1),
+        this->getCluster());
+    Label title(this->getCluster(), "GAME ROOMS", vector2D(320, 20),
+        vector2D(2, 2), color_e::GREEN);
+    // body
+    Button newGame(this->getCluster(), "NEW GAME", vector2D(20, 80),
+        vector2f(2, 2), std::make_shared<SelectScene>(ClusterName::ROOM_LIST));
+    ImageView listBack(listBackgroundPath, vector2D(50, 170), vector2f(1, 1),
+        this->getCluster());
 
+    // footer
+    Label mentionLabel(this->getCluster(), mention, vector2D(290, 780),
+        vector2D(1, 1), color_e::GREEN);
+    Button startButton(this->getCluster(), "Back", vector2D(20, 710),
+        vector2f(2, 2), std::make_shared<SelectPreviousScene>());
+    // SYSTEM SELECTION
     Engine::SystemManager &systemManager = GameCore::engine.getSystemManager();
     systemManager.selectSystems<
         System::RenderSystem,
