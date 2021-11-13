504245640b5fc0c32cfcb1817b26c471e2244c3b
simon.racaud <simon.racaud@epitech.eu>
2021-11-13 15:56:43 +0100

    feat(game-scene): settings scene gui skeleton

--- client/src/Scene/Settings/SettingsScene.cpp
+++ client/src/Scene/Settings/SettingsScene.cpp
@@ -9,7 +9,18 @@
 */
 
 #include "SettingsScene.hpp"
+
 #include "GameCore/GameCore.hpp"
+#include "Item/vector2D.hpp"
+
+#include "Entities/Button/Button.hpp"
+#include "Entities/Label/Label.hpp"
+#include "Entities/ImageView/ImageView.hpp"
+
+#include "Event/GUI/SelectPreviousScene.hpp"
+
+#include "System/RenderSystem/RenderSystem.hpp"
+#include "System/InputEventSystem/InputEventSystem.hpp"
 
 using namespace Scene;
 using namespace Engine;
@@ -22,8 +33,20 @@ SettingsScene::SettingsScene()
 
 void SettingsScene::open()
 {
-    // <== create entities
+    const std::string backgroundPath = GameCore::config->getVar<std::string>("HOME_BACKGROUND");
+    const std::string mention = GameCore::config->getVar<std::string>("HOME_MENTIONS");
+    // ENTITY CREATION
+    ImageView background(backgroundPath, vector2D(0, 0), vector2f(1, 1),
+        this->getCluster());
 
+    Label title(this->getCluster(), "Settings", vector2D(320, 20),
+        vector2D(2, 2), color_e::GREEN);
+    Label mentionLabel(this->getCluster(), mention, vector2D(290, 780),
+        vector2D(1, 1), color_e::GREEN);
+
+    Button startButton(this->getCluster(), "Back", vector2D(20, 710),
+        vector2f(2, 2), std::make_shared<SelectPreviousScene>());
+    // SYSTEM SELECTION
     Engine::SystemManager &systemManager = GameCore::engine.getSystemManager();
     systemManager.selectSystems<
         System::RenderSystem,
