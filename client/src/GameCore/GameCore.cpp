5bade03a1ef689fda04dddf8c05aeabecbfbd69f
simon.racaud <simon.racaud@epitech.eu>
2021-11-14 17:14:49 +0100

    refactor(client): select debug scene

--- client/src/GameCore/GameCore.cpp
+++ client/src/GameCore/GameCore.cpp
@@ -93,7 +93,7 @@ void GameCore::run()
     sceneManager.registerScene<Scene::RoomListScene>();
     sceneManager.registerScene<Scene::GameScene>();
     sceneManager.registerScene<Scene::DebugScene>("Test");
-    sceneManager.select<Scene::GameScene>();
+    sceneManager.select<Scene::DebugScene>();
 
     engine.exec();
 }
