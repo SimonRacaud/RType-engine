dd92b37d8de34a13f1d35087cc9ba4e63fbf6812
Arthur <arthur.jourdan@epitech.eu>
2021-11-14 19:14:38 +0100

    test(lib/Network/tests/src/*/*TCP.cpp): rewrite simple TCP tests with changes that were applied to network classes

--- lib/Network/tests/functional/src/server/testServerTCP.cpp
+++ lib/Network/tests/functional/src/server/testServerTCP.cpp
@@ -15,21 +15,39 @@ using namespace Network;
 class intWrapper {
   public:
     intWrapper() = default;
-    intWrapper(int val) : _val(val){};
+    explicit intWrapper(int val) : _val(val){};
     ~intWrapper() = default;
 
-    std::size_t length() const
+    [[nodiscard]] std::size_t length() const
     {
         return sizeof(intWrapper);
     }
 
-    int getVal() const
+    [[nodiscard]] int getVal() const
     {
         return _val;
     }
-    int getOtherVal() const
+    [[nodiscard]] int getOtherVal() const
     {
         return _otherVal;
+    }
+
+    [[nodiscard]] uint8_t *serialize() const
+    {
+        auto data(new uint8_t[sizeof(intWrapper)]);
+
+        memcpy(data, &_val, sizeof(int));
+        memcpy(data + sizeof(int), &_otherVal, sizeof(int));
+        return data;
+    }
+    intWrapper(uint8_t *data, const std::size_t len)
+    {
+        if (len != sizeof(intWrapper)) {
+            std::cout << "size not good" << std::endl;
+            return;
+        }
+        memcpy(&_val, data, sizeof(int));
+        memcpy(&_otherVal, data + sizeof(int), sizeof(int));
     }
 
   private:
@@ -45,23 +63,23 @@ class intWrapper {
  */
 int testServerAcceptReceive()
 {
-    //    const std::size_t portServer(8080);
-    //    std::tuple<intWrapper, std::size_t, std::string, std::size_t> recvData;
-    //    AsioServerTCP<intWrapper> server(portServer);
-    //    intWrapper my_var;
-    //
-    //    while (true) {
-    //        recvData = server.receiveAny();
-    //        if (std::get<1>(recvData)) {
-    //            my_var = std::get<0>(recvData);
-    //            std::cout << "my_var.getVal() : " << my_var.getVal() << std::endl;
-    //            std::cout << "my_var.getOhterVal() : " << my_var.getOtherVal() << std::endl;
-    //            std::cout << std::endl;
-    //            return 0;
-    //        }
-    //         todo set clock to avoid infinite loop
-    //  in shell script ?
-    //  with Clock class ?
-    //}
+    const std::size_t portServer(8080);
+    std::tuple<intWrapper, std::size_t, std::string, std::size_t> recvData;
+    AsioServerTCP<intWrapper> server(portServer);
+    intWrapper my_var;
+
+    while (true) {
+        recvData = server.receiveAny();
+        if (std::get<1>(recvData)) {
+            my_var = std::get<0>(recvData);
+            std::cout << "my_var.getVal() : " << my_var.getVal() << std::endl;
+            std::cout << "my_var.getOhterVal() : " << my_var.getOtherVal() << std::endl;
+            std::cout << std::endl;
+            return 0;
+        }
+        //  todo set clock to avoid infinite loop
+        //     in shell script ?
+        //     with Clock class ?
+    }
     return 84;
 }
