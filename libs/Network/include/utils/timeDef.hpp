e1abf6a4884c708ddf33a6f13c5af171f5272a16
simon.racaud <simon.racaud@epitech.eu>
2021-11-15 15:53:04 +0100

    feat(network): add macro to get current timestamp

--- libs/Network/include/utils/timeDef.hpp
+++ libs/Network/include/utils/timeDef.hpp
@@ -18,4 +18,8 @@ using DurationCast = std::chrono::duration<Time>;
 using steadyClock = std::chrono::steady_clock;
 using TimePoint = std::chrono::time_point<steadyClock>;
 
+#define GET_NOW (std::chrono::duration_cast<std::chrono::milliseconds>( \
+            std::chrono::steady_clock::now().time_since_epoch()      \
+        ).count())
+
 #endif // TIME_HPP
