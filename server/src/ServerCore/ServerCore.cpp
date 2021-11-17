33db0e883638cdf9b96c2d05f1659f9495e440c6
Sullivan Geslot <sullivan.geslot@epitech.eu>
2021-11-17 11:18:16 +0100

    - Add ServerNetworkCore variable

--- server/src/ServerCore/ServerCore.cpp
+++ server/src/ServerCore/ServerCore.cpp
@@ -9,24 +9,22 @@
 #include "ConfigFileExternal/ConfigFileExternal.hpp"
 
 std::unique_ptr<ConfigFile> ServerCore::config = std::make_unique<ConfigFile>("server.config");
+std::unique_ptr<IServerNetworkCore> ServerCore::network = std::make_unique<ServerNetworkCore>();
 
 ServerCore::ServerCore() :
 _loop(true),
 _rooms()
 {
     config->setVarGetter<std::pair<int, int>>(ConfigFileExternal::getPairIntInt);
-
-    // TODO create TCP server with port var
 }
 
 ServerCore::~ServerCore()
 {
-    // TODO close TCP server
 }
 
 void ServerCore::run(void)
 {
     while (_loop) {
-        // TODO MAIN SERVER && DISPATCHER
+        // TODO
     }
 }
