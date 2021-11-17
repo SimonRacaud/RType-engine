4028ba59ea16f542944a9123d09f93c8a72e8409
Arthur <arthur.jourdan@epitech.eu>
2021-11-17 21:08:54 +0100

    fix(libs/Network/include/ANetwork.hpp): delete[] instead of delete

--- libs/Network/include/ANetwork.hpp
+++ libs/Network/include/ANetwork.hpp
@@ -66,7 +66,7 @@ namespace Network
 
         virtual ~AAsioConnection()
         {
-            delete _recvBuf.first;
+            delete[] _recvBuf.first;
             stopRunAsync();
             disconnectAll();
         }
