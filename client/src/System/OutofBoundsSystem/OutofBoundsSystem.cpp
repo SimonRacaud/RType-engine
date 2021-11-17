9671ea8d32989275fd84c9134a8833b5b57e52ac
Pol Bachelin <pol.bachelin@epitech.eu>
2021-11-17 19:31:06 +0100

    fix(OutofBoundsSystem): bullets not going out the window

--- client/src/System/OutofBoundsSystem/OutofBoundsSystem.cpp
+++ client/src/System/OutofBoundsSystem/OutofBoundsSystem.cpp
@@ -12,7 +12,7 @@ using namespace System;
 static const Engine::Time freq = static_cast<Engine::Time>(10);
 OutofBoundsSystem::OutofBoundsSystem() : Engine::AbstractSystem<OutofBoundsSystem>(freq, SystemPriority::LOW)
 {
-	this->setRequirements<Engine::Position, Engine::Velocity>();
+	this->setRequirements<Engine::Position, Engine::Velocity, Component::EntityMask>();
 }
 
 void OutofBoundsSystem::run(const std::vector<Engine::Entity> &entities)
@@ -23,6 +23,8 @@ void OutofBoundsSystem::run(const std::vector<Engine::Entity> &entities)
 	for (const Engine::Entity &e : entities) {
 		auto [position, velocity] = GET_COMP_M.getList<Engine::Position, Engine::Velocity>(e);
 		
+		if (GET_COMP_M.get<Component::EntityMask>(e)._currentMask != Component::MASK::PLAYER)
+			continue;
 		if ((position.x + playerSize.x > windowSize.x && velocity.x > 0) || (position.x - playerSize.x < 0 && velocity.x < 0))
 			velocity.x = 0;
 		if ((position.y + playerSize.y > windowSize.y && velocity.y > 0) || (position.y - playerSize.y < 0 && velocity.y < 0))
