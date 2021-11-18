96d1c5efc96c22740498f4c5bbd212dcf4cfdb95
simon.racaud <simon.racaud@epitech.eu>
2021-11-18 12:15:05 +0100

    fix(AnimationInfo): path length verification

--- include/Components/AnimationInfo.hpp
+++ include/Components/AnimationInfo.hpp
@@ -8,28 +8,31 @@
 #ifndef ANIMATIONINFO_HPP_
 #define ANIMATIONINFO_HPP_
 
+#include <cstring>
 #include "BaseComponent/BaseComponent.hpp"
 #include "Item/vector2D.hpp"
-#include <cstring>
 
-namespace Component {
-	class AnimationInfo : public Engine::BaseComponent<AnimationInfo> {
-		public:
-			AnimationInfo(std::string path, int nbFrames, vector2D animPos, vector2D animSize)
-				: _nbFrames(nbFrames), _animPos(animPos), _animSize(animSize) {
-					try {
-						std::memcpy(_path, path.c_str(), path.length());
-					} catch (...) {
-						throw Engine::FatalErrorException("Could not copy animation path, because it is too long");
-					}
-				}
-			virtual ~AnimationInfo() = default;
+namespace Component
+{
+#define ANIM_INFO_PATH_SIZE 128
 
-			char _path[128];
-			int _nbFrames;
-			vector2D _animPos;
-			vector2D _animSize;
-	};
-}
+    class AnimationInfo : public Engine::BaseComponent<AnimationInfo> {
+      public:
+        AnimationInfo(std::string path, int nbFrames, vector2D animPos, vector2D animSize)
+            : _nbFrames(nbFrames), _animPos(animPos), _animSize(animSize)
+        {
+            if (_path.size() >= ANIM_INFO_PATH_SIZE) {
+                throw Engine::FatalErrorException("AnimationInfo : Could not copy animation path, because it's too long");
+            }
+            std::memcpy(_path, path.c_str(), path.length());
+        }
+        virtual ~AnimationInfo() = default;
+
+        char _path[ANIM_INFO_PATH_SIZE];
+        int _nbFrames;
+        vector2D _animPos;
+        vector2D _animSize;
+    };
+} // namespace Component
 
 #endif /* !ANIMATIONINFO_HPP_ */
