e0a6b29932b12e390fd317e19d13512a648baf16
Pol Bachelin <pol.bachelin@epitech.eu>
2021-11-18 12:00:49 +0100

    fix(AnimationInfo): fix for animation info serializaton, not using std::string

--- include/Components/AnimationInfo.hpp
+++ include/Components/AnimationInfo.hpp
@@ -10,15 +10,18 @@
 
 #include "BaseComponent/BaseComponent.hpp"
 #include "Item/vector2D.hpp"
+#include <stdlib.h>
 
 namespace Component {
 	class AnimationInfo : public Engine::BaseComponent<AnimationInfo> {
 		public:
 			AnimationInfo(std::string path, int nbFrames, vector2D animPos, vector2D animSize)
-				: _path(path), _nbFrames(nbFrames), _animPos(animPos), _animSize(animSize) {}
+				: _nbFrames(nbFrames), _animPos(animPos), _animSize(animSize) {
+					_path = strdup(path.c_str());
+				}
 			virtual ~AnimationInfo() = default;
 
-			std::string _path;
+			char *_path;
 			int _nbFrames;
 			vector2D _animPos;
 			vector2D _animSize;
