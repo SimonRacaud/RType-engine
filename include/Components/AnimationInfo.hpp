9ffa713b36dda487f58c0e121516edef9429347c
Pol Bachelin <pol.bachelin@epitech.eu>
2021-11-18 12:04:35 +0100

    fix(AnimationInfo): added exeption if path is too long

--- include/Components/AnimationInfo.hpp
+++ include/Components/AnimationInfo.hpp
@@ -10,18 +10,22 @@
 
 #include "BaseComponent/BaseComponent.hpp"
 #include "Item/vector2D.hpp"
-#include <stdlib.h>
+#include <cstring>
 
 namespace Component {
 	class AnimationInfo : public Engine::BaseComponent<AnimationInfo> {
 		public:
 			AnimationInfo(std::string path, int nbFrames, vector2D animPos, vector2D animSize)
 				: _nbFrames(nbFrames), _animPos(animPos), _animSize(animSize) {
-					_path = strdup(path.c_str());
+					try {
+						std::memcpy(_path, path.c_str(), path.length());
+					} catch (...) {
+						throw Engine::FatalErrorException("Could not copy animation path, because it is too long");
+					}
 				}
 			virtual ~AnimationInfo() = default;
 
-			char *_path;
+			char _path[128];
 			int _nbFrames;
 			vector2D _animPos;
 			vector2D _animSize;
