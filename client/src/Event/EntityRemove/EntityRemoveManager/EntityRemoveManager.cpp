bf955f28f67c0f6df9f9695ff3e6adc180f5883a
Pol Bachelin <pol.bachelin@epitech.eu>
2021-11-21 17:53:42 +0100

    feat(EntityRemove): chaning to endGame once every player has died

--- client/src/Event/EntityRemove/EntityRemoveManager/EntityRemoveManager.cpp
+++ client/src/Event/EntityRemove/EntityRemoveManager/EntityRemoveManager.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "EntityRemoveManager.hpp"
+#include "Scene/EndGame/EndGameScene.hpp"
 
 EntityRemoveManager::EntityRemoveManager()
 {
@@ -15,10 +16,22 @@ EntityRemoveManager::EntityRemoveManager()
 
 void entityRemove(const EntityRemoveEvent *e)
 {
+	Engine::ComponentManager &componentManager = GameCore::engine.getComponentManager();
+	std::vector<Engine::Entity> list;
+
     try {
+		if (GET_COMP_M.get<Component::EntityMask>(e->_entity)._currentMask == Component::MASK::PLAYER) {
+			componentManager.foreachComponent<Component::EntityMask>([&list, componentManager](std::unique_ptr<Component::EntityMask> &comp) {
+				Engine::Entity e = componentManager.getOwner(*comp);
+				list.push_back(e);
+			});
+			if (list.size() == 1) {
+				GET_SCENE_M.select<Scene::EndGameScene>(true);
+			}
+		}
         if (GameCore::networkCore.isMaster()) {
             GameCore::networkCore.destroyEntity(GET_ENTITY_M.getNetworkId(e->_entity));
             GET_ENTITY_M.remove(e->_entity);
-        }
+		}
     } catch (std::exception) {}
 }
